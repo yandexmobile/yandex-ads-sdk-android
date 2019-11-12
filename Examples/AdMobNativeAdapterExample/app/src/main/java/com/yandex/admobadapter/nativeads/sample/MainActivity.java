6ac15b5bc33264bc986aaa273a45b3c8ce83de9b
Aliaksei Katsura <katsura-a@yandex-team.ru>
2019-11-12 13:43:03 +0300

    release-admob-mobileads-0.2.0

--- Examples/AdMobNativeAdapterExample/app/src/main/java/com/yandex/admobadapter/nativeads/sample/MainActivity.java
+++ Examples/AdMobNativeAdapterExample/app/src/main/java/com/yandex/admobadapter/nativeads/sample/MainActivity.java
@@ -9,7 +9,7 @@
 package com.yandex.admobadapter.nativeads.sample;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
